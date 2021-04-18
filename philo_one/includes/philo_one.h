f109bf0f5c19b5873f9aa6be5bbdd496ad0827a9
Whitney Phylicia <wphylici@ox-g5.msk.21-school.ru>
2021-04-18 20:03:29 +0300

    fixed die threads and, print time and add eat counter.

--- philo_one/includes/philo_one.h
+++ philo_one/includes/philo_one.h
@@ -6,11 +6,11 @@
 /*   By: wphylici <wphylici@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/14 00:44:11 by wphylici          #+#    #+#             */
-/*   Updated: 2021/02/26 15:47:21 by wphylici         ###   ########.fr       */
+/*   Updated: 2021/04/18 17:55:14 by wphylici         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILO_ONE_H  // ?
+#ifndef PHILO_ONE_H
 # define PHILO_ONE_H
 
 # include <stdio.h>
@@ -19,6 +19,25 @@
 # include <stdlib.h>
 # include <sys/time.h>
 
-int	ft_atoi(const char *str);
+typedef struct		s_philo
+{
+	int				num_of_philo;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				h_m_must_eat;
+	int				count_eat;
+	unsigned int	n;
+	unsigned int 	left_fork;
+    unsigned int	right_fork;
+	unsigned int	last_eat_mutex;
+	size_t			time_last_eat;
+	size_t			start_time;
+	pthread_t 		*t;
+	pthread_mutex_t	*m;
+
+}					t_philo;
+
+int					ft_atoi(const char *str);
 
 #endif
--- philo_one/sources/philo_one.c
+++ philo_one/sources/philo_one.c
@@ -6,33 +6,23 @@
 /*   By: wphylici <wphylici@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/14 00:40:46 by wphylici          #+#    #+#             */
-/*   Updated: 2021/02/28 18:36:43 by wphylici         ###   ########.fr       */
+/*   Updated: 2021/04/18 19:43:57 by wphylici         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo_one.h"
 
-typedef struct		s_philo
-{
-	int				num_of_philo;
-	int				time_to_die;
-	int				time_to_eat;
-	int				time_to_sleep;
-	int				h_m_must_eat;
-	unsigned int	n;
-	unsigned int 	left_fork;
-    unsigned int	right_fork;
-	pthread_t 		*t;
-	pthread_mutex_t	*m;
-
-}					t_philo;
+int				flag_print;
 
 void	parse(t_philo *ph, char **argv)
 {
 	ph->num_of_philo = ft_atoi(argv[1]);
-	ph->time_to_die = ft_atoi(argv[2]) * 1000;
-	ph->time_to_eat = ft_atoi(argv[3]) * 1000;
-	ph->time_to_sleep = ft_atoi(argv[4]) * 1000;
+	ph->time_to_die = ft_atoi(argv[2]);
+	ph->time_to_eat = ft_atoi(argv[3]);
+	ph->time_to_sleep = ft_atoi(argv[4]);
+	ph->last_eat_mutex = ph->num_of_philo;
+	flag_print = 0;
+	ph->count_eat = 0;
 	// ph->h_m_must_eat = ft_atoi(argv[5]);
 	// if (ph->num_of_philo < 2 || ph->time_to_die < 1 ||
 	// 	ph->time_to_eat < 1 || ph->time_to_sleep < 1 ||
@@ -40,76 +30,129 @@ void	parse(t_philo *ph, char **argv)
 	// 	printf("error: incorrect format argument\n");
 }
 
-void	time_die(t_philo *ph)
-{
-	static time_t	str_time = 0;
-	static int		count = 0;
+// int upgrade_usleep(size_t msec)
+// {
+// 	size_t start;
+// 	size_t stop;
+// 	size_t diff;
 
-	if (time(0) - str_time >= 1)
-	{
-		str_time = time(0);
-	}
-	else
-		count++;
-}
+// 	start = get_time();
+// 	stop = get_time();
+// 	diff = stop - start;
+// 	while (diff < msec)
+// 	{
+// 		stop = get_time();
+// 		diff = stop - start;
+// 		usleep(10);
+// 	}
+// 	return (0);
+// }
 
-int		check_time(t_philo *ph)
+size_t	get_time(void)
 {
 	struct timeval tv;
-	struct timezone tz;
-	static time_t time_start;
 
-	gettimeofday(&tv,  &tz);
-	time_start = tv.tv_usec;
-	if (tv.tv_usec - time_start > ph->time_to_die)
+	if (gettimeofday(&tv, NULL) == -1)
+		return (0);
+	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
+}
+
+void	print_logs(char *str, t_philo *ph)
+{
+	if (!flag_print)
+		printf("[%lu] ph %d %s\n", get_time() - ph->start_time, ph->n, str);
+}
+
+int	check_die(t_philo *ph)
+{
+	size_t tmp;
+
+	if (!ph->count_eat)
+		tmp = 0;
+	else
+		tmp = get_time() - ph->time_last_eat;
+	// printf("%d - tmp - %lu\n", ph->n, tmp);
+	if (get_time() < ph->time_last_eat)
+		printf("lalala");
+	if (ph->time_to_die < tmp)
+	{
+		// printf("%u - get time - %lu\n", ph->n, get_time());
+		// printf("%u - last eat time - %lu\n", ph->n, ph->time_last_eat);
+
+		// printf("%d - tmp - %lu\n", ph->n, tmp);
+		print_logs("is dead", ph);
+		flag_print = 1;
 		return (-1);
+	}
+	usleep(50);
 	return (0);
 }
 
+int upgrade_usleep(size_t msec, t_philo *ph)
+{
+	size_t start;
 
-void	*proc(void *str)
+	start = get_time();
+	while (get_time() - start < msec)
+	{
+		// if (check_die(ph))
+		// {
+		// 	return (1);
+		// }
+		usleep(100);
+	}
+	return (0);
+}
+
+void	*proc(void *ptr)
 {
 	int i;
 	t_philo *ph;
 
-	ph = (t_philo *)str;
+	ph = (t_philo *)ptr;
+	ph->time_last_eat = ph->start_time;
 	while (1)
 	{
-		if (check_time(ph))
-		{
-			printf("ph %d is dead\n", ph->n);
-			exit(0);
-		}
 		pthread_mutex_lock(&ph->m[ph->left_fork]);
+		print_logs("take left fork", ph);
 		pthread_mutex_lock(&ph->m[ph->right_fork]);
+		print_logs("take right fork", ph);
 
-		printf("ph %d take fork\n", ph->n);
-		printf("ph %d is eating\n", ph->n);
+		// pthread_mutex_lock(&ph->m[ph->last_eat_mutex]);
+		// pthread_mutex_unlock(&ph->m[ph->last_eat_mutex]);
+		// printf("%d - last meal time - %lu\n", ph->n, ph->time_last_eat - ph->start_time);
+		++ph->count_eat;
 
-		usleep(ph->time_to_eat);
+		print_logs("is eating", ph);
+		ph->time_last_eat = get_time();
+
+		upgrade_usleep(ph->time_to_eat, ph);
+
 		pthread_mutex_unlock(&ph->m[ph->right_fork]);
 		pthread_mutex_unlock(&ph->m[ph->left_fork]);
 
-		printf("ph %d finished dinner\n", ph->n);
-		printf("ph %d is sleeping\n", ph->n);
-		usleep(ph->time_to_sleep);
+		print_logs("finished dinner", ph);
+		print_logs("is sleeping", ph);
+
+		upgrade_usleep(ph->time_to_sleep, ph);
+		//printf("ph %d is thinking\n", ph->n);
 	}
-	return NULL;
+	return (NULL);
 }
 
-void	init_mutex(t_philo *ph)
+void	init_mutex(t_philo **ph)
 {
 	int i;
 
 	i = 0;
-	while (i < ph->num_of_philo)
+	while (i <	(*ph)->num_of_philo)
 	{
-		pthread_mutex_init(&ph->m[i], NULL);
+		pthread_mutex_init(&(*ph)->m[i], NULL);
 		i++;
 	}
 }
 
-void	init_philo(t_philo *ph)
+void	init_philo(t_philo **ph)
 {
 	int i;
 	int l;
@@ -118,12 +161,12 @@ void	init_philo(t_philo *ph)
 	i = 0;
 	l = 0;
 	r = 1;
-	while (i < ph->num_of_philo)
+	while (i < (*ph)->num_of_philo)
 	{
-		ph[i].left_fork = l;
-		ph[i].right_fork = r;
+		(*ph)[i].left_fork = l;
+		(*ph)[i].right_fork = r;
 		l++;
-		if (r == ph->num_of_philo - 1)
+		if (r == (*ph)->num_of_philo - 1)
 			r = 0;
 		else
 			r++;
@@ -131,19 +174,52 @@ void	init_philo(t_philo *ph)
 	}
 }
 
+void	*die(void *ptr)
+{
+	int i;
+	t_philo *ph;
+
+	ph = (t_philo *)ptr;
+
+	while (1)
+	{
+		if (check_die(ph))
+		{
+			// pthread_mutex_unlock(&ph->m[ph->left_fork]);
+			exit(0);
+		}
+	}
+	return (NULL);
+}
+
 void	start(t_philo *ph)
 {
+	size_t time;
 	int	i;
+	// pthread_t *t = malloc(sizeof(pthread_t) * ph->num_of_philo);
 
 	i = 0;
 	while (i < ph->num_of_philo)
 	{
+		if (i == 0)
+			time = get_time();
 		ph[i].n = i;
+		ph[i].start_time = time;
+		if (i % 2 == 1)
+			usleep(10);
 		pthread_create(&ph->t[i], NULL, proc, (void *)&ph[i]);
-		usleep(1000);
+		pthread_create(&ph->t[i], NULL, die, (void *)&ph[i]);
 		i++;
 	}
 	i = 0;
+	// while (i < ph->num_of_philo)
+	// {
+	// 	ph[i].n = i;
+	// 	pthread_create(&ph->t[i], NULL, die, (void *)&ph[i]);
+	// 	upgrade_usleep(10, ph);
+	// 	i++;
+	// }
+	// i = 0;
 	while (i < ph->num_of_philo)
 	{
 		pthread_join(ph->t[i], NULL);
@@ -151,21 +227,21 @@ void	start(t_philo *ph)
 	}
 }
 
-void	init_struct(t_philo *ph, char **argv)
+void	init_struct(t_philo **ph, char **argv)
 {
 	int i;
 	int num_ph;
 
 	i = 0;
 	num_ph = ft_atoi(argv[1]);
-	if (!(ph->t = (pthread_t *)malloc(sizeof(pthread_t) * num_ph)))
+	if (!((*ph)->t = (pthread_t *)malloc(sizeof(pthread_t) * num_ph)))
 		exit(EXIT_FAILURE);
-	if (!(ph->m = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * num_ph)))
+	if (!((*ph)->m = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * num_ph + 1)))
 		exit(EXIT_FAILURE);
 	while (i < num_ph)
 	{
-		ph[i].m = ph[0].m;
-		parse(&ph[i++], argv);
+		(*ph)[i].m = (*ph)[0].m;
+		parse(&(*ph)[i++], argv);
 	}
 	init_mutex(ph);
 	init_philo(ph);
@@ -183,7 +259,7 @@ int		main(int argc, char **argv)
 		exit(EXIT_FAILURE);
 	if (argc > 1)
 	{
-		init_struct(ph, argv);
+		init_struct(&ph, argv);
 		start(ph);
 	}
 	return (0);
